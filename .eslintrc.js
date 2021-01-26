515b5b728f82cbbf9c53ce27f512a324e5fac201
muyeyong <xuyongshuaige@gmail.com>
2021-01-26 15:12:17 +0800

    格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,39 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["plugin:vue/essential", "airbnb-base", "prettier"],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    parser: "@typescript-eslint/parser",
-    sourceType: "module"
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".vue"],
-        moduleDirectory: ["node_modules", "src/"]
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+        moduleDirectory: ['node_modules', 'src/'],
       },
       alias: {
-        map: [["components", "./src/components"]],
-        extensions: [".js", ".vue"]
-      }
-    }
+        map: [['components', './src/components']],
+        extensions: ['.js', '.vue'],
+      },
+    },
   },
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never"
-      }
-    ]
-  }
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,41 @@
 module.exports = {
-  printWidth: 80, //一行的字符数，如果超过会进行换行，默认为80
-  tabWidth: 2, //一个tab代表几个空格数，默认为8
-  useTabs: false, //是否使用tab进行缩进，默认为false，表示用空格进行缩减
-  singleQuote: false, //字符串是否使用单引号，默认为false，使用双引号
-  semi: false, // 行位是否使用分号，默认为true
-  trailingComma: "none", //是否使用尾逗号，有三个可选值"<none|es5|all>"
-  bracketSpacing: true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
-  parser: "babylon" //代码的解析引擎，默认为babylon，与babel相同。
-};
+  // 一行最多 150 字符
+  printWidth: 150,
+  // 使用 2 个空格缩进
+  tabWidth: 4,
+  // 不使用缩进符，而使用空格
+  useTabs: false,
+  // 行尾需要有分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 对象的 key 仅在必要时用引号
+  quoteProps: 'as-needed',
+  // jsx 不使用单引号，而使用双引号
+  jsxSingleQuote: false,
+  // 末尾需要有逗号
+  trailingComma: 'all',
+  // 大括号内的首尾需要空格
+  bracketSpacing: true,
+  // jsx 标签的反尖括号需要换行
+  jsxBracketSameLine: false,
+  // 箭头函数，只有一个参数的时候，也需要括号
+  arrowParens: 'always',
+  // 每个文件格式化的范围是文件的全部内容
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  // 不需要写文件开头的 @prettier
+  requirePragma: false,
+  // 不需要自动在文件开头插入 @prettier
+  insertPragma: false,
+  // 使用默认的折行标准
+  proseWrap: 'preserve',
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'css',
+  // vue 文件中的 script 和 style 内不用缩进
+  vueIndentScriptAndStyle: false,
+  // 换行符使用 lf
+  endOfLine: 'lf',
+  // 格式化嵌入的内容
+  embeddedLanguageFormatting: 'auto',
+}
