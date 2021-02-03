1a2dd5ef115fd1e87d960396b3c368aa5b93bfee
muyeyong <xuyongshuaige@gmail.com>
2021-02-03 10:11:09 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="home">
-        <one-button type="primary"> Primary </one-button><br />
-        <router-link to="/aaa"> 切换子应用Vue2.0</router-link><br />
-        <router-link to="/bbb"> 切换子应用Vue3.0</router-link><br />
         <HelloWorld />
     </div>
 </template>
