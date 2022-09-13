6b032185f1faba698e58d56b13a6f5a20199078e
muyeyong <xuyongshuaige@gmail.com>
2022-09-13 14:07:15 +0800

    feat: 动态导入module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,20 @@
 import { createStore } from 'vuex'
 
+const modules: { [key: string]: any } = {}
+
+const files = require.context('./modules', true, /\.ts/)
+
+files.keys().forEach((key) => {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const module = require(`./modules${key.replace('.', '')}`)
+    const matchResult = key.match(/\.\/(\S*)\.ts/)
+    const moduleName = (matchResult && matchResult[1]) || ''
+    modules[moduleName] = module
+})
+
 export default createStore({
     state: {},
     mutations: {},
     actions: {},
-    modules: {},
+    modules,
 })
